051ab014e100a9e24027cbeb2587fbfa2e5615d7
koalter <heavens_drive090@hotmail.com>
2023-09-06 01:57:09 -0300

    Ionic Components and Modals

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
 
-  constructor() { }
+  readonly iterable: any[] = Array(10);
+  users: any[] = [];
+  loading: boolean = true;
+  selectedUser: any | undefined;
+  page: Element | null = null;
+  activeSegment: string = 'details';
 
-  ngOnInit() {
+  constructor(private alertController: AlertController,
+    private toastController: ToastController) { }
+
+  async ngOnInit() {
+    this.users = await this.getUsers();
+    this.loading = false;
+    this.page = document.querySelector('.page');
   }
 
+  async getUsers(): Promise<any> {
+    const data = await fetch('https://randomuser.me/api?results=10');
+    const json = await data.json();
+    return json.results || [];
+  }
+
+  async clearList(): Promise<void> {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Are you sure you want to delete all users?',
+      buttons: [
+        { text: 'Cancel', role: 'cancel' },
+        {
+          text: 'Delete',
+          handler: async () => {
+            this.users = [];
+            const toast = await this.toastController.create({
+              message: 'All users deleted',
+              duration: 2000,
+              color: 'danger'
+            });
+
+            await toast.present();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  refresh(event: any) {
+    this.loading = true;
+    this.getUsers().then(users => {
+      this.users = users;
+      this.loading = false;
+    }).finally(() => {
+      event.detail.complete();
+    });
+  }
+
+  changeSegment(event: any) {
+    this.activeSegment = event.detail.value;
+  }
 }
