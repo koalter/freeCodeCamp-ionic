93cc36b6c7931a3090b33c22f988f490c049528c
koalter <heavens_drive090@hotmail.com>
2023-09-07 22:51:32 -0300

    Ionic Animations API

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Animation, AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: './tab2.page.html',
   styleUrls: ['./tab2.page.scss'],
 })
-export class Tab2Page implements OnInit {
+export class Tab2Page implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @ViewChild('button', { read: ElementRef }) button!: ElementRef<HTMLIonButtonElement>;
+  private animation!: Animation;
+
+  constructor(private animationCtrl: AnimationController) { }
+  
+  ngAfterViewInit(): void {
+    this.animation = this.animationCtrl.create()
+      .addElement(this.button.nativeElement)
+      .duration(2000)
+      .iterations(Infinity)
+      .delay(1000)
+      .keyframes([
+        { offset: 0, transform: 'scale(1)', opacity: '1' },
+        { offset: 0.5, transform: 'scale(1.5)', opacity: '0.5' },
+        { offset: 1, transform: 'scale(1)', opacity: '1' }
+      ]);
+    
+    this.animation.play();
+  }
 
   ngOnInit() {
   }
