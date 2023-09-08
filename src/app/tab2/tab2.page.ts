2296372346eb8039a7fd4eb008b6f8540e425fe6
koalter <heavens_drive090@hotmail.com>
2023-09-07 23:16:37 -0300

    Ionic Gesture API

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { Animation, AnimationController } from '@ionic/angular';
+import { Animation, AnimationController, GestureController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -9,26 +9,52 @@ import { Animation, AnimationController } from '@ionic/angular';
 export class Tab2Page implements OnInit, AfterViewInit {
 
   @ViewChild('button', { read: ElementRef }) button!: ElementRef<HTMLIonButtonElement>;
-  private animation!: Animation;
-
-  constructor(private animationCtrl: AnimationController) { }
+  @ViewChild('div', { read: ElementRef }) div!: ElementRef<HTMLDivElement>;
+  
+  constructor(private animationCtrl: AnimationController,
+    private gestureCtrl: GestureController) { }
   
   ngAfterViewInit(): void {
-    this.animation = this.animationCtrl.create()
+    this.startAnimation();
+    this.startGesture();
+  }
+
+  ngOnInit() {
+  }
+
+  async startAnimation() {
+    const animation = this.animationCtrl.create()
       .addElement(this.button.nativeElement)
       .duration(2000)
       .iterations(Infinity)
-      .delay(1000)
       .keyframes([
         { offset: 0, transform: 'scale(1)', opacity: '1' },
         { offset: 0.5, transform: 'scale(1.5)', opacity: '0.5' },
         { offset: 1, transform: 'scale(1)', opacity: '1' }
       ]);
     
-    this.animation.play();
+    animation.play();
   }
 
-  ngOnInit() {
-  }
+  async startGesture() {
+    const gesture = this.gestureCtrl.create({
+      el: this.div.nativeElement,
+      gestureName: 'example',
+      onStart: () => {
+        this.div.nativeElement.style.transition = 'none';
+      },
+      onMove: (detail) => {
+        const x = detail.currentX - detail.startX;
+        const y = detail.currentY - detail.startY;
 
+        this.div.nativeElement.style.transform = `translate(${x}px, ${y}px)`;
+      },
+      onEnd: () => {
+        this.div.nativeElement.style.transform = '';
+        this.div.nativeElement.style.transition = '500ms ease-out';
+      }
+    });
+
+    gesture.enable();
+  }
 }
